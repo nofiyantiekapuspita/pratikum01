6632b32a5eefae8e41d16d651f47a7b766c583c8
nofiyantiekapuspita <64140272+nofiyantiekapuspita@users.noreply.github.com>
2020-04-22 21:21:59 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Nofiyanti";
+        String namaTengah = "Eka";
+        String namaBelakang = "Puspita";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 4.98764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
